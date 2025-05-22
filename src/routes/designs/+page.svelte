2c46732efd592e9fe6be9de7ff722070754672f1
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2025-05-22 22:51:30 +0200

    chore: add husky and check linting

--- src/routes/designs/+page.svelte
+++ src/routes/designs/+page.svelte
@@ -9,7 +9,7 @@
 <div
 	class="container mx-auto grid grid-cols-1 gap-8 px-6 py-8 md:grid-cols-2 md:py-16 lg:grid-cols-3"
 >
-	{#each designs as design}
+	{#each designs as design (design.id)}
 		<DesignCard {design} />
 	{/each}
 </div>
