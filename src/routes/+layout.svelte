c8f29f00de40903084cb46a64c5b55c5616f06e9
borisnliscool <boris@borisnl.nl>
2026-01-31 13:55:00 +0100

    chore: add plausible analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,22 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Privacy-friendly analytics by Plausible -->
+	<script async src="https://analytics.boris.foo/js/pa-Ul6KBzU414tYXDh26k8ME.js"></script>
+	<script>
+		((window.plausible =
+			window.plausible ||
+			function () {
+				(plausible.q = plausible.q || []).push(arguments);
+			}),
+			(plausible.init =
+				plausible.init ||
+				function (i) {
+					plausible.o = i || {};
+				}));
+		plausible.init();
+	</script>
 </svelte:head>
 
 <div class="min-h-dvh bg-neutral-50 text-neutral-950">
